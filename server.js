9fb9338f14f2543b72ea33e01a6e975e2ff307b7
Matt Moe <mattmoe@sbcglobal.net>
2020-09-28 21:01:39 -0500

    code

--- server.js
+++ server.js
@@ -43,6 +43,10 @@ var characters = [
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "view.html"));
 });
 
